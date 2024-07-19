42ad34c0c0492fe355ce30fb73dbc447df1f9de0
bonuKoo <evlogiaseio@gmail.com>
2024-07-19 12:26:41 +0900

    2024-07-19 12:25 PM
    네이버 API Map 사용
    [Hello]
    기존 Hello
    엔티티 및
    관련 코드 삭제
    [HTML]
    hello.html
    네이버 지도 우선 추가
    
    by : BonuKoo

--- src/main/java/com/myboard/toy/domain/hello/controller/HelloControllerTest.java
+++ src/main/java/com/myboard/toy/domain/hello/controller/HelloControllerTest.java
@@ -2,6 +2,7 @@ package com.myboard.toy.domain.hello.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 //@Controller
@@ -13,84 +14,10 @@ public class HelloControllerTest {
     private final FileStore fileStore;
     */
 
-    //@GetMapping("")
+    @GetMapping("/hello")
     public String hello(){
         return "hello";
     }
-    
-    // 새로운 Hello 객체를 만들기 위해 form 반환
-    //@GetMapping("/new")
-    /*
-    private String newHello(@ModelAttribute HelloForm form, RedirectAttributes  redirectAttributes) throws IOException {
-        return "hello/hello-form";
-    }
 
-     */
-
-    /*
-        파일 등록 기능
-     */
-    /*
-
-    //@PostMapping("/new")
-    public String saveHello(@ModelAttribute HelloForm form,RedirectAttributes redirectAttributes) throws IOException {
-
-        UploadFileOfBoard attachFile = fileStore.storeFile(form.getAttachFile());
-
-        List<UploadFileOfBoard> storeImageFiles = fileStore.storeFiles(form.getImageFiles());
-
-        //DB에 저장
-
-        Hello hello = new Hello(form.getHelloName(), attachFile,storeImageFiles);
-
-        helloRepository.save(hello);
-
-        redirectAttributes.addAttribute("helloId", hello.getId());
-
-        return "redirect:/hello/{helloId}";
-    }
-    */
-
-    /*
-        조회 시 출력
-     */
-    /*
-
-    //@GetMapping("/{id}")
-    public String hellos(@PathVariable Long id, Model model){
-        Hello hello = helloRepository.findById(id);
-        model.addAttribute("hello",hello);
-        return "hello/hello-view";
-    }
-     */
-
-    /*
-
-    //@ResponseBody
-    //@GetMapping("/images/{filename}")
-    public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
-        return new UrlResource("file:"+fileStore.getFullPath(filename));
-    }
-    */
-    /*
-    //@GetMapping("/attach/{id}")
-    private ResponseEntity<Resource> downloadAttach(@PathVariable Long id) throws MalformedURLException {
-
-        Hello hello = helloRepository.findById(id);
-
-        String storeFileName = hello.getAttachFile().getStoreFileName();
-        String uploadFileName = hello.getAttachFile().getUploadFileName();
-
-        UrlResource resource = new UrlResource("file:" + fileStore.getFullPath(storeFileName));
-
-        log.info("uploadFileName={}",uploadFileName);
-
-        String encodedUploadFileName = UriUtils.encode(uploadFileName, StandardCharsets.UTF_8);
-        String contentDisposition = "attachment; filename=\"" + encodedUploadFileName + "\"";
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION,contentDisposition)
-                .body(resource);
-    }
-    */
 }
 
