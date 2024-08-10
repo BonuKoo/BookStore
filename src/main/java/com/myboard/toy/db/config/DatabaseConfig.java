ee0a63dd66be2b6be71b57ceca02713d3de54484
BonuKoo <evlogiaseio@gmail.com>
2024-08-10 23:50:43 +0900

    상품 개발 2024-08-10 23:50 PM
    DatabaseConfig
    추후 기능 추가

--- src/main/java/com/myboard/toy/db/config/DatabaseConfig.java
+++ src/main/java/com/myboard/toy/db/config/DatabaseConfig.java
@@ -3,10 +3,12 @@ package com.myboard.toy.db.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Configuration
+//@Configuration
+/*
 @EnableJpaRepositories
         (
                 basePackages = ""
         )
+*/
 public class DatabaseConfig {
 }
